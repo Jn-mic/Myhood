ee153008b042e10735f133170d5256cb921f29e6
jn-mic <jackotienokey@gmail.com>
2021-07-25 18:49:51 +0300

    initial view fnx upon authentication

--- myneighbour/views.py
+++ myneighbour/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from __future__ import unicode_literals
+
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .forms import *
+from .models import *
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.models import User
 
 # Create your views here.
+def index(request):
+    try:
+        if not request.user.is_authenticated:
+            return redirect('/accounts/login/')
+        current_user=request.user
+        profile =Profile.objects.get(username=current_user)
+    except ObjectDoesNotExist:
+        return redirect('create-profile')
