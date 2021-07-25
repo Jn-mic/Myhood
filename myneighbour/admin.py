6d29d0ca6bbfda6bced7065ac721c48830bc6303
jn-mic <jackotienokey@gmail.com>
2021-07-25 18:46:22 +0300

    register models cls on admin.py

--- myneighbour/admin.py
+++ myneighbour/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from __future__ import unicode_literals
+from .models import neighbourhood, Profile, Authorities, Health, Post
 
 # Register your models here.
+admin.site.register(neighbourhood)
+admin.site.register(Profile)
+admin.site.register(Authorities)
+admin.site.register(Health)
+admin.site.register(Post)
