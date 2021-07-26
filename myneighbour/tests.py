6f99e77d75db1fd6b99ac1857c98d9e879027531
jn-mic <jackotienokey@gmail.com>
2021-07-26 09:55:10 +0300

    creating a test instance for my neighhood & asserting True

--- myneighbour/tests.py
+++ myneighbour/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import neighbourhood
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class neighbourhoodTestClass(TestCase):
+    def setUp(self):
+        self.Langata = neighbourhood(neighbourhood='Langata')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Langata,neighbourhood))
